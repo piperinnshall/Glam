b467fcdc27f78ce5b13287c218fd54d77686d052
piperinnshall <piper.innshall@gmail.com>
2025-08-02 10:59:56 +1200

    chore: Styling

--- app/src/main/java/piperinnshall/glam/lexer/TokenizeFile.java
+++ app/src/main/java/piperinnshall/glam/lexer/TokenizeFile.java
@@ -10,6 +10,7 @@ import piperinnshall.glam.tuple.Token;
 
 public interface TokenizeFile {
   abstract BufferedReader r();
+
   default LinkedList<Token> tokenize() throws IOException {
     int[] i = { 1 };
     return r().lines()
@@ -18,16 +19,20 @@ public interface TokenizeFile {
           return line(line, y);
         }).reduce((LinkedListEmpty<Token>) l -> l, LinkedList::concat);
   }
+
   private static LinkedList<Token> line(String line, int y) {
     return lineHelper(line, 0, y, (LinkedListEmpty<Token>) l -> l)
         .add(Token.of(TokenType.NEWLINE, "\n", 0, line.length()))
         .reverse();
   }
+
   private static LinkedList<Token> lineHelper(String line, int x, int y, LinkedList<Token> acc) {
-    if (x >= line.length()) return acc; 
+    if (x >= line.length())
+      return acc;
     Token token = findAnyToken(line, x, y);
     return lineHelper(line, x + token.lexeme().length(), y, acc.add(token));
   }
+
   private static Token findAnyToken(String line, int x, int y) {
     return findPresetToken(line, x, y)
         .or(() -> findIdentf(line, x, y))
@@ -35,52 +40,73 @@ public interface TokenizeFile {
         .or(() -> findLitStr(line, x, y))
         .orElseGet(() -> Token.of(TokenType.INVALID, line.substring(x, x + 1), y, x));
   }
+
   private static Optional<Token> findPresetToken(String line, int x, int y) {
     return findPresetToken(line, x, y, TokenType.longestTokenLength());
   }
+
   private static Optional<Token> findPresetToken(String line, int x, int y, int len) {
-    if (len == 0) return Optional.empty();
-    if (x + len > line.length()) return findPresetToken(line, x, y, len - 1);
+    if (len == 0)
+      return Optional.empty();
+    if (x + len > line.length())
+      return findPresetToken(line, x, y, len - 1);
     String sub = line.substring(x, x + len);
     TokenType type = TokenType.fromString(sub);
-    if (type != null) return Optional.of(Token.of(type, sub, y, x));
+    if (type != null)
+      return Optional.of(Token.of(type, sub, y, x));
     return findPresetToken(line, x, y, len - 1);
   }
+
   private static Optional<Token> findIdentf(String line, int x, int y) {
-    if (!Character.isLetter(line.charAt(x))) return Optional.empty();
+    if (!Character.isLetter(line.charAt(x)))
+      return Optional.empty();
     String lexeme = findIdentf(line, x);
     return Optional.of(Token.of(TokenType.IDENTIFIER, lexeme, y, x));
   }
+
   private static String findIdentf(String line, int pos) {
     if (pos < line.length() && Character.isLetterOrDigit(line.charAt(pos)))
       return line.charAt(pos) + findIdentf(line, pos + 1);
     return "";
   }
+
   private static Optional<Token> findLitNum(String line, int x, int y) {
-    if (!Character.isDigit(line.charAt(x))) return Optional.empty();
+    if (!Character.isDigit(line.charAt(x)))
+      return Optional.empty();
     String lexeme = findLitNum(line, x, false);
     return Optional.of(Token.of(TokenType.LIT_NUM, lexeme, y, x));
   }
+
   private static String findLitNum(String line, int pos, boolean dot) {
     if (pos < line.length()) {
       char c = line.charAt(pos);
-      if (Character.isDigit(c)) return c + findLitNum(line, pos + 1, dot);
-      if (c == '.' && !dot) return c + findLitNum(line, pos + 1, true);
+      if (Character.isDigit(c))
+        return c + findLitNum(line, pos + 1, dot);
+      if (c == '.' && !dot)
+        return c + findLitNum(line, pos + 1, true);
     }
     return "";
   }
+
   private static Optional<Token> findLitStr(String line, int x, int y) {
-    if (line.charAt(x) != '"') return Optional.empty();
+    if (line.charAt(x) != '"')
+      return Optional.empty();
     return findLitStr(line, x + 1, y, x, 0, false, false, "\"");
   }
+
   private static Optional<Token> findLitStr(String line, int pos, int y, int start, int brace, boolean escaped,
       boolean invalid, String acc) {
-    if (pos >= line.length()) return Optional.of(Token.of(TokenType.LIT_STR_INVALID, acc, y, start));
+    if (pos >= line.length())
+      return Optional.of(Token.of(TokenType.LIT_STR_INVALID, acc, y, start));
     char c = line.charAt(pos);
-    if (escaped) return findLitStr(line, pos + 1, y, start, brace, false, invalid, acc + c);
-    if (c == '\\') return findLitStr(line, pos + 1, y, start, brace, true, invalid, acc + c);
-    if (c == '{') return findLitStr(line, pos + 1, y, start, brace + 1, false, invalid, acc + c);
-    if (c == '}') return findLitStr(line, pos + 1, y, start, brace - 1, false, invalid || brace == 0, acc + c);
+    if (escaped)
+      return findLitStr(line, pos + 1, y, start, brace, false, invalid, acc + c);
+    if (c == '\\')
+      return findLitStr(line, pos + 1, y, start, brace, true, invalid, acc + c);
+    if (c == '{')
+      return findLitStr(line, pos + 1, y, start, brace + 1, false, invalid, acc + c);
+    if (c == '}')
+      return findLitStr(line, pos + 1, y, start, brace - 1, false, invalid || brace == 0, acc + c);
     if (c == '"') {
       TokenType type = (brace == 0 && !invalid) ? TokenType.LIT_STR : TokenType.LIT_STR_INVALID;
       return Optional.of(Token.of(type, acc + c, y, start));
